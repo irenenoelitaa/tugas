177deac20b6f65efe5093fa85fa0137116ef72f1
irenenoelitaa <khususfoto1910@gmail.com>
2025-02-19 13:42:00 +0700

    Add files via upload

---
+++ data.php
@@ -0,0 +1,234 @@
+<html>
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <link rel="shortcut icon" href="logo.png" type="image/x-icon">
+  <title>Akadem-Angkatan-laut</title>
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,600,700&display=swap" rel="stylesheet" />
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+  <link rel="stylesheet" href = "capaa.css">
+</head>
+
+<body>
+  <div class="hero_area">
+    <!-- header section strats -->
+    <div class="hero_bg_box">
+      <div class="img-box">
+        <img src="AAL.jpeg" alt="">
+      </div>
+    </div>
+    <header class="header_section">
+      <div class="header_top">
+        <div class="container-fluid">
+          <div class="contact_link-container">
+            <a href="" class="contact_link1">
+              <i class="fa fa-map-marker" aria-hidden="true"></i>
+              <span>
+                Jalasveva Jayamahe
+              </span>
+            </a>
+            <a href="" class="contact_link2">
+              <i class="fa fa-phone" aria-hidden="true"></i>
+              <span>
+                (031) 3291092
+              </span>
+            </a>
+            <a href="" class="contact_link3">
+              <i class="fa fa-envelope" aria-hidden="true"></i>
+              <span>
+                depmarinir_aal@tnial.mil.id
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="header_bottom">
+        <div class="container-fluid">
+          <nav class="navbar navbar-expand-lg custom_nav-container">
+            <a class="navbar-brand" href="index.html">
+              <span>
+               Akademi Angkatan Laut 
+              </span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class=""></span>
+            </button>
+
+            <div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
+              <ul class="navbar-nav  ">
+                <li class="nav-item ">
+                  <a class="nav-link" href="index.php">Beranda <span class="sr-only">(current)</span></a>
+                </li><li class="nav-item ">
+                  <a class="nav-link" href="about.php">About <span class="sr-only">(current)</span></a>
+                </li><li class="nav-item ">
+                  <a class="nav-link" href="daftar.php">Pendaftaran <span class="sr-only">(current)</span></a>
+                </li><li class="nav-item ">
+                  <a class="nav-link" href="data.php">Data Pendaftaran <span class="sr-only">(current)</span></a>
+                </li>
+              
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+    <!-- end header section -->
+
+  <!-- Bagian Nama Nama Kelompok -->
+
+  <section class="about_section layout_padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 px-0">
+          <div class="img_container">
+            <div class="img-box">
+              <img src="foto1.jpeg" alt="" />
+			 </div>
+          </div>
+        </div>
+        <div class="col-md-6 px-0">
+          <div class="detail-box">
+            <div class="heading_container ">
+              <h2>
+                DATA PENDAFTTAR
+              </h2>
+			  <table border="1">
+			  
+			  <th>Nama Lengkap</th>
+			  <th>NISN</th>
+			  <th>Asal Sekolah</th>
+			  <th>Tempat Lahir</th>
+			  <th>Tanggal Lahir</th>
+			  <th>Alamat</th>
+			  <th>Asal Kota</th>
+			  <th>Jenis Kelamin</th>
+			  <th>Email</th>
+			  <th>Nilai s1</th>
+			  <th>Nilai s2</th>
+			  <th>Nilai s3</th>
+			  <th>Nilai s4</th>
+			  <th>Nilai s5</th>
+			  
+			  <!-- Ardi -->
+			  <tr>
+			  <td>Muhammad Ardi</td>
+			  <td>NISN</td>
+			  <td>SMAN 4 BEKASI</td>
+			  <td>Boyolali</td>
+			  <td>14/04/2007</td>
+			  <td>Alamat</td>
+			  <td>Bekasi</td>
+			  <td>Laki-Laki</td>
+			  <td>Email</td>
+			  <td>60</td>
+			  <<td>79</td>
+			  <td>88</td>
+			  <td>87</td>
+			  <td>89</td>
+			  </tr>
+			  
+			  <!-- Nanda -->
+			  <tr>
+			  <td>Muhammad Nanda</td>
+			  <td>NISN</td>
+			  <td>SMAN 4 BEKASI</td>
+			  <td>Bekasi</td>
+			  <td>07/12/2006</td>
+			  <td>Alamat</td>
+			  <td>Bekasi</td>
+			  <td>Laki-Laki</td>
+			  <td>Email</td>
+			  <td>89</td>
+			  <<td>88</td>
+			  <td>90</td>
+			  <td>88</td>
+			  <td>87</td>
+			  </tr>
+			  
+			  <!-- Ardi -->
+			  <tr>
+			  <td>Raasyid Azka</td>
+			  <td>NISN</td>
+			  <td>SMAN 4 BEKASI</td>
+			  <td>Tempat Lahir</td>
+			  <td>Tanggal Lahir</td>
+			  <td>Alamat</td>
+			  <td>Asal Kota</td>
+			  <td>Jenis Kelamin</td>
+			  <td>Email</td>
+			  <td>87</td>
+			  <<td>88</td>
+			  <td>89</td>
+			  <td>87</td>
+			  <td>89</td>
+			  </tr>
+			  
+			  <!-- Ardi -->
+			  <tr>
+			  <td>Zaid Alfandi</td>
+			  <td>NISN</td>
+			  <td>SMAN 4 BEKASI</td>
+			  <td>Bekasi</td>
+			  <td>02/02/2007</td>
+			  <td>jln.cempaka 1</td>
+			  <td>BEKASI</td>
+			  <td>Laki Laki</td>
+			  <td>zaid.alfandi@gmail.com</td>
+			  <td>87</td>
+			  <<td>86</td>
+			  <td>88</td>
+			  <td>89</td>
+			  <td>88</td>
+			  </tr>
+			  
+			  <!-- Ardi -->
+			  <tr>
+			  <td>Fardhan </td>
+			  <td>NISN</td>
+			  <td>SMAN 4 BEKASI</td>
+			  <td>Tempat Lahir</td>
+			  <td>Tanggal Lahir</td>
+			  <td>Alamat</td>
+			  <td>Asal Kota</td>
+			  <td>Jenis Kelamin</td>
+			  <td>Email</td>
+			  <td>89</td>
+			  <<td>87</td>
+			  <td>88</td>
+			  <td>86</td>
+			  <td>87</td>
+			  </tr>
+            </div>
+           
+						
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<!-- Bagian Nama Nama Kelompok -->
+
+  <!-- footer section -->
+  <footer class="container-fluid footer_section">
+    <p>
+      &copy; <span id="currentYear"></span> SMAN 4 Bekasi
+      <a href="https://html.design/">Kelompok....</a>
+    </p>
+  </footer>
+  <!-- footer section -->
+</body>
+</html>
