dccd5f91659a234e6d32b4ca5bcd917dfdd33b1b
irenenoelitaa <khususfoto1910@gmail.com>
2025-02-19 13:36:30 +0700

    Add files via upload

---
+++ AAL/daftar.php
@@ -0,0 +1,216 @@
+<html>
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <link rel="shortcut icon" href="logo.png" type="image/x-icon">
+  <title>Stip-about</title>
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,600,700&display=swap" rel="stylesheet" />
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+  <div class="hero_area">
+    <!-- header section strats -->
+    <div class="hero_bg_box">
+      <div class="img-box">
+        <img src="AAL.jpeg" alt="">
+      </div>
+    </div>
+    <header class="header_section">
+      <div class="header_top">
+        <div class="container-fluid">
+          <div class="contact_link-container">
+            <a href="" class="contact_link1">
+              <i class="fa fa-map-marker" aria-hidden="true"></i>
+              <span>
+                Jalasveva Jayamahe
+              </span>
+            </a>
+            <a href="" class="contact_link2">
+              <i class="fa fa-phone" aria-hidden="true"></i>
+              <span>
+                (031) 3291092
+              </span>
+            </a>
+            <a href="" class="contact_link3">
+              <i class="fa fa-envelope" aria-hidden="true"></i>
+              <span>
+                depmarinir_aal@tnial.mil.id
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="header_bottom">
+        <div class="container-fluid">
+          <nav class="navbar navbar-expand-lg custom_nav-container">
+            <a class="navbar-brand" href="index.html">
+              <span>
+               Akademi Angakatan Laut
+              </span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class=""></span>
+            </button>
+
+            <div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
+              <ul class="navbar-nav  ">
+                <li class="nav-item ">
+                  <a class="nav-link" href="index.php">Beranda <span class="sr-only">(current)</span></a>
+                </li><li class="nav-item ">
+                  <a class="nav-link" href="about.php">About <span class="sr-only">(current)</span></a>
+                </li><li class="nav-item ">
+                  <a class="nav-link" href="daftar.php">Pendaftaran <span class="sr-only">(current)</span></a>
+                </li><li class="nav-item ">
+                  <a class="nav-link" href="data.php">Data Pendaftaran <span class="sr-only">(current)</span></a>
+                </li>
+              
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+    <!-- end header section -->
+
+  <!-- Bagian Nama Nama Kelompok -->
+
+  <section class="about_section layout_padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 px-0">
+          <div class="img_container">
+            <div class="img-box">
+              <img src="gedung.jpg" alt="" />
+			 </div>
+          </div>
+        </div>
+        <div class="col-md-6 px-0">
+          <div class="detail-box">
+            <div class="heading_container ">
+              <h2>
+                formulir pendaftaran
+              </h2>
+			  <form action="" method="POST">
+ 			  <table border="1">
+			  <tr>
+			  <td> Nama Lengkap</td>
+			  <td>:</td>
+			  <td><input type="text" name="namalengkap"></td>
+			  </tr>
+			  <tr>
+			  <td> NISN</td>
+			  <td>:</td>
+			  <td><input type="text" name="nisn"></td>
+			  </tr>
+			  <tr>
+			  <td> Asal Sekolah</td>
+			  <td>:</td>
+			  <td><input type="text" name="asalsekolah"></td>
+			  </tr>
+			  <tr>
+			  <td> Tempat Lahir</td>
+			  <td>:</td>
+			  <td><input type="text" name="tempatlahir"></td>
+			  </tr>
+			  <tr>
+			  <td> Tanggal Lahir</td>
+			  <td>:</td>
+			  <td><input type="date" name="tanggallahir"></td>
+			  </tr>
+			  <tr>
+			  <td> Alamat</td>
+			  <td>:</td>
+			  <td><input type="text" name="alamat"></td>
+			  </tr>
+			  <tr>
+			  <td> Asal Kota</td>
+			  <td>:</td>
+			  <td><select name="kota">
+			  <option value="Bekasi">Bekasi</option>
+			  <option value="Jakarta">Jakarta</option>
+			  <option value="Bandung">Bandung</option>
+			  </select>
+			  </td>
+			  </tr>
+			  <tr>
+			  <td>Jenis Kelamin</td>
+			  <td>:</td>
+			  <td><input type="radio" name="jeniskelamin" value="laki-laki" required>Laki-Laki
+			  <input type="radio" name="jeniskelamin" value="Perempuan" required>Perempuan</td>
+			  </td>
+			  <tr>
+			  <td> Email</td>
+			  <td>:</td>
+			  <td><input type="email" name="email"></td>
+			  </tr>
+			  <tr>
+			  <td> Nomor HP</td>
+			  <td>:</td>
+			  <td><input type="text" name="nohp"></td>
+			  </tr>
+			  <tr>
+			  <td> Nilai s1</td>
+			  <td>:</td>
+			  <td><input type="number" name="nilais1"></td>
+			  </tr>
+			  <tr>
+			  <td> Nilai s2</td>
+			  <td>:</td>
+			  <td><input type="number" name="nilais2"></td>
+			  </tr>
+			  <tr>
+			  <td> Nilai s3</td>
+			  <td>:</td>
+			  <td><input type="number" name="nilais3"></td>
+			  </tr>
+			  <tr>
+			  <td> Nilai s4</td>
+			  <td>:</td>
+			  <td><input type="number" name="nilais4"></td>
+			  </tr>
+			  <tr>
+			  <td> Nilai s5</td>
+			  <td>:</td>
+			  <td><input type="number" name="nilais5"></td>
+			  </tr>
+			  <tr>
+			  <td><input type="submit" name="submit" value="daftar">
+			  <input type="reset" name="reset" value="batal"></td>
+			  </tr>
+			  </table>
+			  </form>
+              </div>
+           
+						
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<!-- Bagian Nama Nama Kelompok -->
+
+  <!-- footer section -->
+  <footer class="container-fluid footer_section">
+    <p>
+      &copy; <span id="currentYear"></span> SMAN 4 Bekasi
+      <a href="https://html.design/">Kelompok....</a>
+    </p>
+  </footer>
+  <!-- footer section -->
+</body>
+</html>
